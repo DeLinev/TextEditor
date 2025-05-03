1de6e946da8a2ede2666507e706ac24a1b102561
Denys <den.linevych@gmail.com>
2025-05-03 22:04:34 +0300

    Add markdown commands

--- TextEditor/Models/Commands/ItalicTextCommand.cs
+++ TextEditor/Models/Commands/ItalicTextCommand.cs
@@ -2,11 +2,11 @@
 
 namespace TextEditor.Models.Commands
 {
-    public class BoldTextCommand : Command
+    public class ItalicTextCommand : Command
     {
         private int start, length;
 
-        public BoldTextCommand(TextBox editText, Document document) : base(editText, document) 
+        public ItalicTextCommand(TextBox editText, Document document) : base(editText, document)
         {
             start = _editTextBox.SelectionStart;
             length = _editTextBox.SelectionLength;
@@ -14,16 +14,16 @@ namespace TextEditor.Models.Commands
 
         public override void Execute()
         {
-            _document.InsertText("**", start + length);
-            _document.InsertText("**", start);
+            _document.InsertText("*", start + length);
+            _document.InsertText("*", start);
             _editTextBox.Text = _document.Content;
-            _editTextBox.CaretIndex = start + length + 2;        
+            _editTextBox.CaretIndex = start + length + 1;
         }
 
         public override void Undo()
         {
-            _document.DeleteText(start + length + 2, 2);
-            _document.DeleteText(start, 2);
+            _document.DeleteText(start + length + 1, 1);
+            _document.DeleteText(start, 1);
             _editTextBox.Text = _document.Content;
             _editTextBox.CaretIndex = start;
         }
--- TextEditor/UserControls/EditUserControl.xaml.cs
+++ TextEditor/UserControls/EditUserControl.xaml.cs
@@ -14,10 +14,11 @@ namespace TextEditor.UserControls
         public EditUserControl(string? path = null)
         {
             InitializeComponent();
-            UndoStack = new Stack<Command>();
             CurrentDocument = new Document(path);
+            UndoStack = new Stack<Command>();
         }
 
+        private bool isExecutingCommand = false;
         public Document CurrentDocument { get; set; }
         public Stack<Command> UndoStack { get; set; }
 
@@ -25,8 +26,11 @@ namespace TextEditor.UserControls
 
         public void ExecuteCommand(Command command)
         {
+            isExecutingCommand = true;
             command.Execute();
+            isExecutingCommand = false;
             UndoStack.Push(command);
+            EditTextBox.Focus();
         }
 
         public void Undo()
@@ -34,8 +38,12 @@ namespace TextEditor.UserControls
             if (UndoStack.Any())
             {
                 var command = UndoStack.Pop();
+
+                isExecutingCommand = true;
                 command.Undo();
+                isExecutingCommand = false;
             }
+            EditTextBox.Focus();
         }
 
         private void PreviewBtn_Click(object sender, RoutedEventArgs e)
@@ -49,18 +57,63 @@ namespace TextEditor.UserControls
             ExecuteCommand(boldCommand);
         }
 
-        private void EditTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        private void ItalicBtn_Click(object sender, RoutedEventArgs e)
         {
-            CurrentDocument.ChangeContent(EditTextBox.Text);
+            var italicCommand = new ItalicTextCommand(EditTextBox, CurrentDocument);
+            ExecuteCommand(italicCommand);
         }
 
-        private void EditTextBox_PreviewKeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        private void HeaderBtn_Click(object sender, RoutedEventArgs e)
         {
+            var headerCommand = new InsertOnStartCommand(EditTextBox, CurrentDocument, '#');
+            ExecuteCommand(headerCommand);
+        }
+
+        private void ListBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var headerCommand = new InsertOnStartCommand(EditTextBox, CurrentDocument, '-');
+            ExecuteCommand(headerCommand);
+        }
+
+        private void UndoBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Undo();
+        }
+
+        private void EditTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (isExecutingCommand)
+                return;
+
+            var editTextCommand = new EditTextCommand(EditTextBox, CurrentDocument);
+            ExecuteCommand(editTextCommand);
+        }
+
+        private void EditTextBox_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            Command command;
+
             if (Keyboard.Modifiers == ModifierKeys.Control && e.Key == Key.B)
             {
-                var boldCommand = new BoldTextCommand(EditTextBox, CurrentDocument);
-                ExecuteCommand(boldCommand);
+                command = new BoldTextCommand(EditTextBox, CurrentDocument);
             }
+            else if (Keyboard.Modifiers == ModifierKeys.Control && e.Key == Key.I)
+            {
+                command = new ItalicTextCommand(EditTextBox, CurrentDocument);
+            }
+            else if (Keyboard.Modifiers == ModifierKeys.Control && e.Key == Key.Z)
+            {
+                e.Handled = true;
+                Undo();
+                return;
+            }
+            else
+            {
+                return;
+            }
+
+            ExecuteCommand(command);
         }
+
     }
 }
