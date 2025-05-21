84a413330e12a2b3051ce4238373c50b9d7f9ba7
vt231_kso <vt231_kso@student.ztu.edu.ua>
2025-05-21 23:19:56 +0300

    eliminate duplication in text formatting commands using Template Method pattern

--- TextEditor/Models/Commands/BoldTextCommand.cs
+++ TextEditor/Models/Commands/BoldTextCommand.cs
@@ -1,31 +1,12 @@
-﻿using System.Windows.Controls;
+using System.Windows.Controls;
 
 namespace TextEditor.Models.Commands
 {
-    public class BoldTextCommand : Command
-    {
-        private int start, length;
-
-        public BoldTextCommand(TextBox editText, Document document) : base(editText, document) 
-        {
-            start = _editTextBox.SelectionStart;
-            length = _editTextBox.SelectionLength;
-        }
-
-        public override void Execute()
-        {
-            _document.InsertText("**", start + length);
-            _document.InsertText("**", start);
-            _editTextBox.Text = _document.Content;
-            _editTextBox.CaretIndex = start + length + 2;        
-        }
-
-        public override void Undo()
-        {
-            _document.DeleteText(start + length + 2, 2);
-            _document.DeleteText(start, 2);
-            _editTextBox.Text = _document.Content;
-            _editTextBox.CaretIndex = start;
-        }
-    }
+  public class BoldTextCommand : FormattedTextCommand
+  {
+    protected override string OpeningTag => "**";
+    protected override string ClosingTag => "**";
+    public BoldTextCommand(TextBox editText, Document document)
+        : base(editText, document) { }
+  }
 }
--- TextEditor/Models/Commands/ItalicTextCommand.cs
+++ TextEditor/Models/Commands/ItalicTextCommand.cs
@@ -1,31 +1,12 @@
-﻿using System.Windows.Controls;
+using System.Windows.Controls;
 
 namespace TextEditor.Models.Commands
 {
-    public class ItalicTextCommand : Command
-    {
-        private int start, length;
-
-        public ItalicTextCommand(TextBox editText, Document document) : base(editText, document)
-        {
-            start = _editTextBox.SelectionStart;
-            length = _editTextBox.SelectionLength;
-        }
-
-        public override void Execute()
-        {
-            _document.InsertText("*", start + length);
-            _document.InsertText("*", start);
-            _editTextBox.Text = _document.Content;
-            _editTextBox.CaretIndex = start + length + 1;
-        }
-
-        public override void Undo()
-        {
-            _document.DeleteText(start + length + 1, 1);
-            _document.DeleteText(start, 1);
-            _editTextBox.Text = _document.Content;
-            _editTextBox.CaretIndex = start;
-        }
-    }
+  public class ItalicTextCommand : FormattedTextCommand
+  {
+    protected override string OpeningTag => "*";
+    protected override string ClosingTag => "*";
+    public ItalicTextCommand(TextBox editText, Document document)
+        : base(editText, document) { }
+  }
 }
